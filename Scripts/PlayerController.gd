8981d6172a8eeee1ad4ecbe0d803b2a22086c80e
Chris Hunsinger <hunsinger89@gmail.com>
2019-12-29 11:22:28 -0500

    player sprite update
    movement update, new house tiles added, player scene updated, and player controller modified to reflect new sprite animation names.

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -4,6 +4,7 @@ class_name PlayerController
 const MAX_SPEED = 50
 const ACCEL = 350
 var velocity = Vector2()
+var last_dir
 
 
 func _ready():
@@ -20,21 +21,23 @@ func _physics_process(delta):
 	#check vertical movement
 	if Input.is_action_pressed("MoveDown"):
 		velocity.y = min(velocity.y + ACCEL * delta, MAX_SPEED)
-		$AnimatedSprite.play("moving")
+		$AnimatedSprite.play("walk down")
+		last_dir = "down"
 	elif Input.is_action_pressed("MoveUp"):
 		velocity.y = max(velocity.y - ACCEL * delta, -MAX_SPEED)
-		$AnimatedSprite.play("moving")
+		$AnimatedSprite.play("walk up")
+		last_dir = "up"
 	else:
 		velocity.y = 0
 	#check horizontal movement
 	if Input.is_action_pressed("MoveLeft"):
 		velocity.x = max(velocity.x - ACCEL * delta, -MAX_SPEED)
-		$AnimatedSprite.play("moving")
-		$AnimatedSprite.set_flip_h(false)
+		$AnimatedSprite.play("walk left")
+		last_dir = "left"
 	elif Input.is_action_pressed("MoveRight"):
 		velocity.x = min(velocity.x + ACCEL * delta, MAX_SPEED)
-		$AnimatedSprite.play("moving")
-		$AnimatedSprite.set_flip_h(true)
+		$AnimatedSprite.play("walk right")
+		last_dir = "right"
 	else:
 		velocity.x = 0
 	
@@ -44,5 +47,25 @@ func _physics_process(delta):
 		velocity.y = 0
 	
 	if velocity.x == 0 and velocity.y == 0:
-		$AnimatedSprite.play("idle")
+		if last_dir == "left":
+			$AnimatedSprite.play("idle left")
+		elif last_dir == "right":
+			$AnimatedSprite.play("idle right")
+		elif last_dir == "up":
+			$AnimatedSprite.play("idle up")
+		elif last_dir == "down":
+			$AnimatedSprite.play("idle down")
+		else:
+			$AnimatedSprite.play("idle down")
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 		
