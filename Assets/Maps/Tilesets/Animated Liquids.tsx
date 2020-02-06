fa87956f771064521f97b30a40cefb2aedd428e0
Chris Hunsinger <hunsinger89@gmail.com>
2020-02-05 22:55:43 -0500

    new assets and animated tiles

---
+++ Assets/Maps/Tilesets/Animated Liquids.tsx
@@ -0,0 +1,147 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="Animated Liquids" tilewidth="16" tileheight="16" tilecount="144" columns="16">
+ <image source="Animated Liquids.png" width="256" height="144"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="2" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="41" duration="100"/>
+   <frame tileid="25" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="10">
+  <animation>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="43" duration="100"/>
+   <frame tileid="27" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <animation>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="29" duration="100"/>
+   <frame tileid="45" duration="100"/>
+   <frame tileid="29" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="16">
+  <animation>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="18" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="20">
+  <animation>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="23" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="32">
+  <animation>
+   <frame tileid="33" duration="100"/>
+   <frame tileid="34" duration="100"/>
+   <frame tileid="35" duration="100"/>
+   <frame tileid="34" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="36">
+  <animation>
+   <frame tileid="37" duration="100"/>
+   <frame tileid="38" duration="100"/>
+   <frame tileid="39" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="52">
+  <animation>
+   <frame tileid="53" duration="100"/>
+   <frame tileid="54" duration="100"/>
+   <frame tileid="55" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="57">
+  <animation>
+   <frame tileid="73" duration="100"/>
+   <frame tileid="89" duration="100"/>
+   <frame tileid="105" duration="100"/>
+   <frame tileid="89" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="58">
+  <animation>
+   <frame tileid="74" duration="100"/>
+   <frame tileid="90" duration="100"/>
+   <frame tileid="106" duration="100"/>
+   <frame tileid="90" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="59">
+  <animation>
+   <frame tileid="75" duration="100"/>
+   <frame tileid="91" duration="100"/>
+   <frame tileid="107" duration="100"/>
+   <frame tileid="91" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="60">
+  <animation>
+   <frame tileid="76" duration="100"/>
+   <frame tileid="92" duration="100"/>
+   <frame tileid="108" duration="100"/>
+   <frame tileid="92" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="68">
+  <animation>
+   <frame tileid="69" duration="100"/>
+   <frame tileid="70" duration="100"/>
+   <frame tileid="71" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="84">
+  <animation>
+   <frame tileid="85" duration="100"/>
+   <frame tileid="86" duration="100"/>
+   <frame tileid="87" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="100">
+  <animation>
+   <frame tileid="101" duration="100"/>
+   <frame tileid="102" duration="100"/>
+   <frame tileid="103" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="116">
+  <animation>
+   <frame tileid="117" duration="100"/>
+   <frame tileid="118" duration="100"/>
+   <frame tileid="119" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="132">
+  <animation>
+   <frame tileid="133" duration="100"/>
+   <frame tileid="134" duration="100"/>
+   <frame tileid="135" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ Assets/Maps/Tilesets/Cliff Masks.tsx
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="Cliff Masks" tilewidth="16" tileheight="16" tilecount="840" columns="28">
+ <image source="Cliff Masks.png" width="448" height="480"/>
+ <terraintypes>
+  <terrain name="Dirt Cliff" tile="84"/>
+  <terrain name="Stone Cliff" tile="89"/>
+  <terrain name="Sandstone Cliff" tile="94"/>
+  <terrain name="Jungle Stone Cliff" tile="99"/>
+  <terrain name="Cliff Fringe" tile="0"/>
+ </terraintypes>
+ <tile id="0" terrain="4,4,4,0"/>
+ <tile id="1" terrain="4,4,0,0"/>
+ <tile id="2" terrain="4,4,0,4"/>
+ <tile id="5" terrain="4,4,4,1"/>
+ <tile id="6" terrain="4,4,1,1"/>
+ <tile id="7" terrain="4,4,1,4"/>
+ <tile id="10" terrain="4,4,4,2"/>
+ <tile id="11" terrain="4,4,2,2"/>
+ <tile id="12" terrain="4,4,2,4"/>
+ <tile id="15" terrain="4,4,4,3"/>
+ <tile id="16" terrain="4,4,3,3"/>
+ <tile id="17" terrain="4,4,3,4"/>
+ <tile id="19" terrain="4,4,4,4"/>
+ <tile id="28" terrain="0,4,0,4"/>
+ <tile id="29" terrain="4,0,0,0"/>
+ <tile id="30" terrain="0,4,0,0"/>
+ <tile id="33" terrain="1,4,1,4"/>
+ <tile id="34" terrain="4,1,1,1"/>
+ <tile id="35" terrain="1,4,1,1"/>
+ <tile id="38" terrain="2,4,2,4"/>
+ <tile id="39" terrain="4,2,2,2"/>
+ <tile id="40" terrain="2,4,2,2"/>
+ <tile id="43" terrain="3,4,3,4"/>
+ <tile id="44" terrain="4,3,3,3"/>
+ <tile id="45" terrain="3,4,3,3"/>
+ <tile id="56" terrain="4,0,4,0"/>
+ <tile id="57" terrain="0,0,4,0"/>
+ <tile id="58" terrain="0,0,0,4"/>
+ <tile id="61" terrain="4,1,4,1"/>
+ <tile id="62" terrain="1,1,4,1"/>
+ <tile id="63" terrain="1,1,1,4"/>
+ <tile id="66" terrain="4,2,4,2"/>
+ <tile id="67" terrain="2,2,4,2"/>
+ <tile id="68" terrain="2,2,2,4"/>
+ <tile id="71" terrain="4,3,4,3"/>
+ <tile id="72" terrain="3,3,4,3"/>
+ <tile id="73" terrain="3,3,3,4"/>
+ <tile id="84" terrain="4,0,4,4"/>
+ <tile id="85" terrain="0,0,4,4"/>
+ <tile id="86" terrain="0,4,4,4"/>
+ <tile id="89" terrain="4,1,4,4"/>
+ <tile id="90" terrain="1,1,4,4"/>
+ <tile id="91" terrain="1,4,4,4"/>
+ <tile id="94" terrain="4,2,4,4"/>
+ <tile id="95" terrain="2,2,4,4"/>
+ <tile id="96" terrain="2,4,4,4"/>
+ <tile id="99" terrain="4,3,4,4"/>
+ <tile id="100" terrain="3,3,4,4"/>
+ <tile id="101" terrain="3,4,4,4"/>
+ <tile id="172" terrain="0,0,0,0"/>
+ <tile id="177" terrain="1,1,1,1"/>
+ <tile id="182" terrain="2,2,2,2"/>
+ <tile id="187" terrain="3,3,3,3"/>
+</tileset>
