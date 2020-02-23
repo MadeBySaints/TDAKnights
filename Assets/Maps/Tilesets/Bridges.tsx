1cce7af568adad3393c5ac5d608810be3de5d534
Chris Hunsinger <hunsinger89@gmail.com>
2020-02-22 23:38:53 -0500

    Massive update to tiles, and a folder shuffle
    Attempted to organize things a little. Theres still a lot of unused content in /Tilesets, but they will be used eventually. Set up a player instance on the first map, so testing can be done. Collisions should be complete, and ysorting should be in order.

--- Assets/Maps/Tilesets/Bridges.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="Bridges" tilewidth="16" tileheight="16" tilecount="48" columns="8">
- <image source="Bridges.png" width="128" height="96"/>
-</tileset>
--- Assets/Maps/Tilesets/CounterAndTableProps.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="CounterAndTableProps" tilewidth="16" tileheight="16" tilecount="200" columns="10">
- <image source="CounterAndTableProps.png" width="160" height="320"/>
-</tileset>
