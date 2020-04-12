e9cff47fe61c2c1a14878e5f1fcd34af8287a660
Chris Hunsinger <hunsinger89@gmail.com>
2020-04-12 13:09:58 -0400

    item db update

--- Core/ItemDB.gd
+++ Core/ItemDB.gd
@@ -10,14 +10,13 @@ class_name ItemDatabase
 
 
 #500-999 = Misc
-
 var coin = [
-	{id = 500, name = "Copper Coin", img = null},
-	{id = 501, name = "Silver Coin", img = null},
-	{id = 502, name = "Gold Coin", img = null},
-	{id = 503, name = "Crypt Coin", img = null},
-	{id = 504, name = "Dragon Coin", img = null},
-	{id = 505, name = "Shard", img = null}
+	{id = 500, name = "Copper Coin", baseVal = 1, img = null},
+	{id = 501, name = "Silver Coin", baseVal = 100, img = null},
+	{id = 502, name = "Gold Coin", baseVal = 10000, img = null},
+	{id = 503, name = "Crypt Coin", baseVal = 0, img = null},
+	{id = 504, name = "Dragon Coin", baseVal = 0, img = null},
+	{id = 505, name = "Shard", baseVal = 0, img = null}
 ]
 
 
@@ -51,41 +50,101 @@ var consumable = [
 	{id = 3001, name = "Pear", hpOnUse = 0, mpOnUse = 10, weight = 1, timer = 0, baseVal = 20, img = "res://Sprites/Consumables/Pear.png"},
 	{id = 3002, name = "HP Vial", hpOnUse = 25, mpOnUse = 0, weight = 1, timer = 0, baseVal = 50, img = "res://Sprites/Consumables/HP Vial.png"},
 	{id = 3003, name = "MP Vial", hpOnUse = 0, mpOnUse = 25, weight = 1, timer = 0, baseVal = 50, img = "res://Sprites/Consumables/MP Vial.png"},
-	{id = 1000, name = "HP Potion", hpOnUse = 50, mpOnUse = 0, weight = 1, timer = 0, baseVal = 100, img = "res://Sprites/Consumables/HP Potion.png"},
-	{id = 3004, name = "MP Potion", hpOnUse = 0, mpOnUse = 50, weight = 1, timer = 0, baseVal = 100, img = "res://Sprites/Consumables/MP Potion.png"},
-	{id = 3005, name = "HP Flask", hpOnUse = 100, mpOnUse = 0, weight = 1, timer = 0, baseVal = 200, img = "res://Sprites/Consumables/HP Flask.png"},
-	{id = 3006, name = "MP Flask", hpOnUse = 0, mpOnUse = 100, weight = 1, timer = 0, baseVal = 200, img = "res://Sprites/Consumables/MP Flask.png"}
+	{id = 3004, name = "HP Potion", hpOnUse = 50, mpOnUse = 0, weight = 1, timer = 0, baseVal = 100, img = "res://Sprites/Consumables/HP Potion.png"},
+	{id = 3005, name = "MP Potion", hpOnUse = 0, mpOnUse = 50, weight = 1, timer = 0, baseVal = 100, img = "res://Sprites/Consumables/MP Potion.png"},
+	{id = 3006, name = "HP Flask", hpOnUse = 100, mpOnUse = 0, weight = 1, timer = 0, baseVal = 200, img = "res://Sprites/Consumables/HP Flask.png"},
+	{id = 3007, name = "MP Flask", hpOnUse = 0, mpOnUse = 100, weight = 1, timer = 0, baseVal = 200, img = "res://Sprites/Consumables/MP Flask.png"}
 ]
 
-#4000-4999 = Ingredients/Components (made from refined mats and other items)
+#4000-4999 = Components (for crafting with blueprints, made from refined mats and other items)
 var component = [
 	{id = 4000, name = "Bow String", baseVal= 6, craft = [
 		{id = 6000, amt = 2}
-	]}
+	]},
+	{id = 4001, name = "Wood Bow Frame", baseVal= 6, craft = [
+		{id = 6015, amt = 3}
+	]},
+	{id = 4002, name = "Wood Arrow Shaft", baseVal= 6, craft = [
+		{id = 6005, amt = 2}
+	]},
+	{id = 4003, name = "Stone Arrow Head", baseVal= 6, craft = [
+		{id = 6001, amt = 2}
+	]},
 ]
 
 #5000-5999 = Raw Materials
 var rawMat = [
-	{id = 5000, name = "Stone", baseVal = 1, img = null},
-	{id = 5001, name = "Plant Fiber", baseVal = 1, img = null},
-	{id = 5002, name = "Iron Ore", baseVal = 1, img = null},
-	{id = 5003, name = "Coal Ore", baseVal = 1, img = null},
-	{id = 5004, name = "Sulfur Ore", baseVal = 1, img = null},
-	{id = 5005, name = "Wood", baseVal = 1, img = null},
-	{id = 5006, name = "Hide", baseVal = 1, img = null},
-	{id = 5007, name = "Sand", baseVal = 1, img = null},
-	{id = 5008, name = "Silver Ore", baseVal = 1, img = null},
-	{id = 5009, name = "Gold Ore", baseVal = 1, img = null},
-	{id = 5010, name = "Silicon Ore", baseVal = 1, img = null},
-	{id = 5011, name = "Magnesium Ore", baseVal = 1, img = null},
-	{id = 5012, name = "Raw Carbon", baseVal = 1, img = null},
-	{id = 5013, name = "Bone", baseVal = 1, img = null}
+	{id = 5000, name = "Stone", baseVal = 1, weight = 1, img = null},
+	{id = 5001, name = "Plant Fiber", baseVal = 1, weight = 1, img = null},
+	{id = 5002, name = "Iron Ore", baseVal = 1, weight = 1, img = null},
+	{id = 5003, name = "Coal Ore", baseVal = 1, weight = 1, img = null},
+	{id = 5004, name = "Sulfur Ore", baseVal = 1, weight = 1, img = null},
+	{id = 5005, name = "Wood", baseVal = 1, weight = 1, img = null},
+	{id = 5006, name = "Hide", baseVal = 1, weight = 1, img = null},
+	{id = 5007, name = "Sand", baseVal = 1, weight = 1, img = null},
+	{id = 5008, name = "Silver Ore", baseVal = 1, weight = 1, img = null},
+	{id = 5009, name = "Gold Ore", baseVal = 1, weight = 1, img = null},
+	{id = 5010, name = "Silicon Ore", baseVal = 1, weight = 1, img = null},
+	{id = 5011, name = "Magnesium Ore", baseVal = 1, weight = 1, img = null},
+	{id = 5012, name = "Raw Carbon", baseVal = 1, weight = 1, img = null},
+	{id = 5013, name = "Bone", baseVal = 1, weight = 1, img = null},
+	{id = 5014, name = "Copper Ore", baseVal = 1, weight = 1, img = null},
+	{id = 5015, name = "Tin Ore", baseVal = 1, weight = 1, img = null},
+	{id = 5016, name = "Platinum Ore", baseVal = 1, weight = 1, img = null},
+	{id = 5017, name = "Cobalt Ore", baseVal = 1, weight = 1, img = null},
+	{id = 5018, name = "Mithril Ore", baseVal = 1, weight = 1, img = null}
 ]
 
-#6000-6999 = Refined Materials (made from raw mats)
+#6000-6999 = Refined Materials (crafted from raw mats)
 var refinedMat = [
-	{id = 6000, name = "Jute Twine", baseVal = 3, craft = [
-		{id = 5001 , amt = 3}
+	{id = 6000, name = "Jute Twine", baseVal = 3, weight = 1, img = null, craft = [
+		{id = 5001, amt = 3}
+	]},
+	{id = 6001, name = "Iron Ingot", baseVal = 3, weight = 1, img = null, craft = [
+		{id = 5002, amt = 3}
+	]},
+	{id = 6002, name = "Copper Ingot", baseVal = 3, weight = 1, img = null, craft = [
+		{id = 5014, amt = 3}
+	]},
+	{id = 6003, name = "Tin Ingot", baseVal = 3, weight = 1, img = null, craft = [
+		{id = 5015, amt = 3}
+	]},
+	{id = 6004, name = "Bronze Ingot", baseVal = 3, weight = 1, img = null, craft = [
+		{id = 5014, amt = 3}, #copper ore
+		{id = 5015, amt = 3} #tin ore
+	]},
+	{id = 6005, name = "Silver Ingot", baseVal = 3, weight = 1, img = null, craft = [
+		{id = 5008, amt = 3}
+	]},
+	{id = 6006, name = "Gold Ingot", baseVal = 3, weight = 1, img = null, craft = [
+		{id = 5009, amt = 3}
+	]},
+	{id = 6007, name = "Platinum Ingot", baseVal = 3, weight = 1, img = null, craft = [
+		{id = 5016, amt = 3}
+	]},
+	{id = 6008, name = "Silicon Ingot", baseVal = 3, weight = 1, img = null, craft = [
+		{id = 5010, amt = 3}
+	]},
+	{id = 6009, name = "Magnesium Ingot", baseVal = 3, weight = 1, img = null, craft = [
+		{id = 5011, amt = 3}
+	]},
+	{id = 6010, name = "Cobalt Ingot", baseVal = 3, weight = 1, img = null, craft = [
+		{id = 5017, amt = 3}
+	]},
+	{id = 6011, name = "Mithril Ingot", baseVal = 3, weight = 1, img = null, craft = [
+		{id = 5018, amt = 3}
+	]},
+	{id = 6012, name = "Bonemeal", baseVal = 3, weight = 1, img = null, craft = [
+		{id = 5013, amt = 3}
+	]},
+	{id = 6013, name = "Leather", baseVal = 3, weight = 1, img = null, craft = [
+		{id = 5006, amt = 3}
+	]},
+	{id = 6014, name = "Sulfur", baseVal = 3, weight = 1, img = null, craft = [
+		{id = 5004, amt = 3}
+	]},
+	{id = 6015, name = "Shaped Wood", baseVal = 3, weight = 1, img = null, craft = [
+		{id = 5005, amt = 3}
 	]}
 ]
 
